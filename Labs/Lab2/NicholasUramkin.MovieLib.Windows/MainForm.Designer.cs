fda49cfee9859ffa0fdaa33a065b7f0b9c5bf0fe
nuramkin <35548874+nuramkin@users.noreply.github.com>
2018-02-26 23:40:21 -0600

    Add files via upload

---
+++ Labs/Lab2/NicholasUramkin.MovieLib.Windows/MainForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace NicholasUramkin.MovieLib.Windows
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._mainMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.productToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.removeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this._mainMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // _mainMenu
+            // 
+            this._mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.productToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this._mainMenu.Location = new System.Drawing.Point(0, 0);
+            this._mainMenu.Name = "_mainMenu";
+            this._mainMenu.Size = new System.Drawing.Size(684, 24);
+            this._mainMenu.TabIndex = 0;
+            this._mainMenu.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.OnFileExit);
+            // 
+            // productToolStripMenuItem
+            // 
+            this.productToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.removeToolStripMenuItem});
+            this.productToolStripMenuItem.Name = "productToolStripMenuItem";
+            this.productToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.productToolStripMenuItem.Text = "&Movies";
+            // 
+            // addToolStripMenuItem
+            // 
+            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
+            this.addToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.addToolStripMenuItem.Text = "&Add";
+            this.addToolStripMenuItem.Click += new System.EventHandler(this.OnMoviesAdd);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.editToolStripMenuItem.Text = "&Edit";
+            this.editToolStripMenuItem.Click += new System.EventHandler(this.OnMoviesEdit);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(114, 6);
+            // 
+            // removeToolStripMenuItem
+            // 
+            this.removeToolStripMenuItem.Name = "removeToolStripMenuItem";
+            this.removeToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.removeToolStripMenuItem.Text = "&Remove";
+            this.removeToolStripMenuItem.Click += new System.EventHandler(this.OnMoviesRemove);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.OnHelpAbout);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 361);
+            this.Controls.Add(this._mainMenu);
+            this.MainMenuStrip = this._mainMenu;
+            this.MinimumSize = new System.Drawing.Size(335, 75);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Movie Library (Nicholas Uramkin)";
+            this._mainMenu.ResumeLayout(false);
+            this._mainMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip _mainMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem productToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem removeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ Labs/Lab2/NicholasUramkin.MovieLib.Windows/MovieDetailForm.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NicholasUramkin.MovieLib.Windows
+{
+    public partial class MovieDetailForm : Form
+    {
+
+        public MovieDetailForm()
+        {
+            InitializeComponent();
+        }
+
+        public MovieDetailForm( string title ) : this()
+        {
+            Text = title;
+        }
+
+        public MovieDetailForm( Movie movie ) : this("Edit Movie")
+        {
+            Movie = movie;
+        }
+
+        public Movie Movie { get; set; }
+
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad(e);
+
+
+
+            //load movie
+            if (Movie != null)
+            {
+                _txtTitle.Text = Movie.Title;
+                _txtDescription.Text = Movie.Description;
+                _txtLength.Text = Movie.Length.ToString();
+                _chkOwned.Checked = Movie.Owned;
+            }
+
+        
+        }
+
+        private void OnSave( object sender, EventArgs e )
+        {
+            //create movie
+            var movie = new Movie();
+            movie.Title = _txtTitle.Text;
+            movie.Description = _txtDescription.Text;
+            movie.Length = ConvertToLength(_txtLength);
+            movie.Owned = _chkOwned.Checked;
+
+            //validate
+            var message = movie.Validate();
+            if (!String.IsNullOrEmpty(message))
+            {
+                DisplayError(message);
+                return;
+            }
+
+            //return from form
+            Movie = movie;
+            DialogResult = DialogResult.OK;
+
+            Close();
+        }
+
+        private int ConvertToLength( TextBox control )
+        {
+            if (Int32.TryParse(control.Text, out var length))
+                return length;
+
+            return -1;
+        }
+
+        private void OnCancel( object sender, EventArgs e )
+        {
+            Close();
+        }
+
+        private void DisplayError( string message )
+        {
+            MessageBox.Show(this, message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void _txtTitle_Validating( object sender, CancelEventArgs e )
+        {
+            var textbox = sender as TextBox;
+
+            if (String.IsNullOrEmpty(textbox.Text))
+            {
+                _errorProvider.SetError(textbox, "Title is required");
+                e.Cancel = true;
+            } else
+                _errorProvider.SetError(textbox, "");
+        }
+
+        private void _txtLength_Validating( object sender, CancelEventArgs e )
+        {
+            var textbox = sender as TextBox;
+
+            var price = ConvertToLength(textbox);
+            if (price < 0)
+            {
+                _errorProvider.SetError(textbox, "Length must be >= 0");
+                e.Cancel = true;
+            } else
+                _errorProvider.SetError(textbox, "");
+        }
+    }
+}
