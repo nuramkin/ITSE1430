0cf03c2bd9c3a789768b0efe1e8083cdcfce62ee
nuramkin <35548874+nuramkin@users.noreply.github.com>
2018-02-26 23:40:21 -0600

    test

---
+++ Labs/Lab2/NicholasUramkin.MovieLib.Windows/MainForm.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NicholasUramkin.MovieLib.Windows
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private Movie _movie;
+
+        private void OnFileExit( object sender, EventArgs e )
+        {
+            Close();
+        }
+
+        private void OnMoviesAdd( object sender, EventArgs e )
+        {
+            var button = sender as ToolStripMenuItem;
+
+            var form = new MovieDetailForm("Add Product");
+
+            //show form modally
+            var result = form.ShowDialog(this);
+            if (result != DialogResult.OK)
+                return;
+
+            //add the movie
+            _movie = form.Movie;
+            MessageBox.Show("Movie Added");
+        }
+
+        private void OnMoviesEdit( object sender, EventArgs e )
+        {
+            if (_movie == null)
+            {
+                MessageBox.Show("No movie in system", "Error");
+                return;
+            }
+            var form = new MovieDetailForm(_movie);
+            form.Movie = _movie;
+
+            //show form modally
+            var result = form.ShowDialog(this);
+            if (result != DialogResult.OK)
+                return;
+
+            //edit the product
+            _movie = form.Movie;
+            MessageBox.Show("Edit Saved");
+        }
+
+        private void OnMoviesRemove( object sender, EventArgs e )
+        {
+            if (_movie == null)
+            {
+                MessageBox.Show("No movie in system", "Error");
+                return;
+            }
+
+            if (!ShowConfirmation("Are you sure?", "Remove Movie"))
+                return;
+
+            //remove movie
+            _movie = null;
+            MessageBox.Show("Movie removed");
+        }
+
+        private void OnHelpAbout( object sender, EventArgs e )
+        {
+
+            var form = new AboutBox();
+
+            form.ShowDialog();
+        }
+
+        private bool ShowConfirmation( string message, string title )
+        {
+            return MessageBox.Show(this, message, title, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes;
+        }
+    }
+}
---
+++ Labs/Lab2/NicholasUramkin.MovieLib/Movie.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NicholasUramkin.MovieLib
+{
+    /// <summary>Provides information about a movie.</summary>
+    public class Movie
+    {
+        /// <summary>Gets or sets title.</summary>
+        public string Title
+        {
+            get { return _title ?? ""; }
+            set { _title = value ?? ""; }
+        }
+
+        /// <summary>Gets or sets description.</summary>
+        public string Description
+        {
+            get { return _description ?? ""; }
+            set { _description = value ?? ""; }
+        }
+
+        /// <summary>Gets or sets length</summary>
+        public int Length { get; set; }
+
+        /// <summary>Gets or sets Owned</summary>
+        public bool Owned { get; set; }
+
+        /// <summary>Validation method for title and length<returns></returns>
+        public string Validate()
+        {
+            //Title is required
+            if (String.IsNullOrEmpty(_title))
+                return "Title cannot be empty";
+
+            //Length >= 0
+            if (Length < 0)
+                return "Length must be >= 0";
+
+            return "";
+        }
+
+        private string _title;
+        private string _description;
+    }
+}
