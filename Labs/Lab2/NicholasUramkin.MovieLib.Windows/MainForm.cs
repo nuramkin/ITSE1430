e17796ef9c5a06b9cc4b93bd89a1f487224790fe
nicholas.uramkin <nicholas.uramkin@NEITD-123502>
2018-02-28 19:40:01 -0600

    test

--- Labs/Lab2/NicholasUramkin.MovieLib.Windows/MainForm.cs
+++ Labs/Lab2/NicholasUramkin.MovieLib.Windows/MainForm.cs
@@ -33,7 +33,7 @@ namespace NicholasUramkin.MovieLib.Windows
 
         private void OnMoviesAdd( object sender, EventArgs e )
         {
-            var button = sender as ToolStripMenuItem;
+            var button = sender as ToolStripMenuItem;//test
 
             var form = new MovieDetailForm("Add Product");
 
