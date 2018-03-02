2b04458b888ec5e3789453bdeaca91e8037207e5
Nick <nicholas.uramkin@my.tccd.edu>
2018-03-02 01:47:26 -0600

    cleaned up/re-commented Lab 2

--- Labs/Lab2/NicholasUramkin.MovieLib.Windows/MainForm.cs
+++ Labs/Lab2/NicholasUramkin.MovieLib.Windows/MainForm.cs
@@ -34,32 +34,40 @@ namespace NicholasUramkin.MovieLib.Windows
 
         private void OnMoviesAdd( object sender, EventArgs e )
         {
-            var button = sender as ToolStripMenuItem;
-
+            //instatiate MovieDetailForm with Add Product in titlebar
             var form = new MovieDetailForm("Add Product");
 
+            //show MovieDetailForm
             var result = form.ShowDialog(this);
+            //return if OK(Save) not selected
             if (result != DialogResult.OK)
                 return;
 
+            //add movie
             _movie = form.Movie;
             MessageBox.Show("Movie Added");
         }
 
         private void OnMoviesEdit( object sender, EventArgs e )
         {
+            //check for movie in system
             if (_movie == null)
             {
                 MessageBox.Show("No movie in system", "Error");
                 return;
             }
+
+            //instatiate MovieDetailForm with movie being edited
             var form = new MovieDetailForm(_movie);
+            
+            //load movie into form
             form.Movie = _movie;
 
             var result = form.ShowDialog(this);
             if (result != DialogResult.OK)
                 return;
 
+            //edit movie(assign edited form to movie)
             _movie = form.Movie;
             MessageBox.Show("Edit Saved");
         }
@@ -72,23 +80,27 @@ namespace NicholasUramkin.MovieLib.Windows
                 return;
             }
 
+            //get confirmation from user
             if (!ShowConfirmation("Are you sure?", "Remove Movie"))
                 return;
 
+            //remove movie
             _movie = null;
             MessageBox.Show("Movie removed");
         }
 
+        //show about window
         private void OnHelpAbout( object sender, EventArgs e )
         {
-
             var form = new AboutBox();
 
             form.ShowDialog();
         }
 
+        //show confirmation window
         private bool ShowConfirmation( string message, string title )
         {
+            //return true if Yes
             return MessageBox.Show(this, message, title, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes;
         }
 
