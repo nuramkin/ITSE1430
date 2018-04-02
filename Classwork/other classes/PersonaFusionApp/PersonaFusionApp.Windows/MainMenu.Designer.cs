fd57019f2b316c072d646fa406513d714c301a65
Nick <nicholas.uramkin@my.tccd.edu>
2018-04-02 10:10:52 -0500

    updated persona app

---
+++ Classwork/other classes/PersonaFusionApp/PersonaFusionApp.Windows/MainMenu.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace PersonaFusionApp
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonFool = new System.Windows.Forms.Button();
+            this.buttonMagician = new System.Windows.Forms.Button();
+            this.buttonPriestess = new System.Windows.Forms.Button();
+            this.buttonEmpress = new System.Windows.Forms.Button();
+            this.buttonEmperor = new System.Windows.Forms.Button();
+            this.buttonHierophant = new System.Windows.Forms.Button();
+            this.buttonLovers = new System.Windows.Forms.Button();
+            this.buttonChariot = new System.Windows.Forms.Button();
+            this.buttonJustice = new System.Windows.Forms.Button();
+            this.buttonHermit = new System.Windows.Forms.Button();
+            this.buttonFortune = new System.Windows.Forms.Button();
+            this.buttonStrength = new System.Windows.Forms.Button();
+            this.buttonHanged = new System.Windows.Forms.Button();
+            this.buttonDeath = new System.Windows.Forms.Button();
+            this.buttonTemperance = new System.Windows.Forms.Button();
+            this.buttonDevil = new System.Windows.Forms.Button();
+            this.buttonTower = new System.Windows.Forms.Button();
+            this.buttonStar = new System.Windows.Forms.Button();
+            this.buttonMoon = new System.Windows.Forms.Button();
+            this.buttonSun = new System.Windows.Forms.Button();
+            this.buttonJudgement = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textSearch = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonFool
+            // 
+            this.buttonFool.Location = new System.Drawing.Point(61, 161);
+            this.buttonFool.Name = "buttonFool";
+            this.buttonFool.Size = new System.Drawing.Size(375, 27);
+            this.buttonFool.TabIndex = 0;
+            this.buttonFool.Text = "Fool";
+            this.buttonFool.UseVisualStyleBackColor = true;
+            // 
+            // buttonMagician
+            // 
+            this.buttonMagician.Location = new System.Drawing.Point(61, 194);
+            this.buttonMagician.Name = "buttonMagician";
+            this.buttonMagician.Size = new System.Drawing.Size(375, 23);
+            this.buttonMagician.TabIndex = 1;
+            this.buttonMagician.Text = "Magician";
+            this.buttonMagician.UseVisualStyleBackColor = true;
+            // 
+            // buttonPriestess
+            // 
+            this.buttonPriestess.Location = new System.Drawing.Point(61, 223);
+            this.buttonPriestess.Name = "buttonPriestess";
+            this.buttonPriestess.Size = new System.Drawing.Size(375, 23);
+            this.buttonPriestess.TabIndex = 2;
+            this.buttonPriestess.Text = "Priestess";
+            this.buttonPriestess.UseVisualStyleBackColor = true;
+            // 
+            // buttonEmpress
+            // 
+            this.buttonEmpress.Location = new System.Drawing.Point(61, 252);
+            this.buttonEmpress.Name = "buttonEmpress";
+            this.buttonEmpress.Size = new System.Drawing.Size(375, 23);
+            this.buttonEmpress.TabIndex = 3;
+            this.buttonEmpress.Text = "Empress";
+            this.buttonEmpress.UseVisualStyleBackColor = true;
+            // 
+            // buttonEmperor
+            // 
+            this.buttonEmperor.Location = new System.Drawing.Point(61, 281);
+            this.buttonEmperor.Name = "buttonEmperor";
+            this.buttonEmperor.Size = new System.Drawing.Size(375, 23);
+            this.buttonEmperor.TabIndex = 4;
+            this.buttonEmperor.Text = "Emperor";
+            this.buttonEmperor.UseVisualStyleBackColor = true;
+            // 
+            // buttonHierophant
+            // 
+            this.buttonHierophant.Location = new System.Drawing.Point(61, 310);
+            this.buttonHierophant.Name = "buttonHierophant";
+            this.buttonHierophant.Size = new System.Drawing.Size(375, 23);
+            this.buttonHierophant.TabIndex = 5;
+            this.buttonHierophant.Text = "Hierophant";
+            this.buttonHierophant.UseVisualStyleBackColor = true;
+            // 
+            // buttonLovers
+            // 
+            this.buttonLovers.Location = new System.Drawing.Point(61, 339);
+            this.buttonLovers.Name = "buttonLovers";
+            this.buttonLovers.Size = new System.Drawing.Size(375, 23);
+            this.buttonLovers.TabIndex = 6;
+            this.buttonLovers.Text = "Lovers";
+            this.buttonLovers.UseVisualStyleBackColor = true;
+            // 
+            // buttonChariot
+            // 
+            this.buttonChariot.Location = new System.Drawing.Point(61, 368);
+            this.buttonChariot.Name = "buttonChariot";
+            this.buttonChariot.Size = new System.Drawing.Size(375, 23);
+            this.buttonChariot.TabIndex = 7;
+            this.buttonChariot.Text = "Chariot";
+            this.buttonChariot.UseVisualStyleBackColor = true;
+            // 
+            // buttonJustice
+            // 
+            this.buttonJustice.Location = new System.Drawing.Point(61, 397);
+            this.buttonJustice.Name = "buttonJustice";
+            this.buttonJustice.Size = new System.Drawing.Size(375, 23);
+            this.buttonJustice.TabIndex = 8;
+            this.buttonJustice.Text = "Justice";
+            this.buttonJustice.UseVisualStyleBackColor = true;
+            // 
+            // buttonHermit
+            // 
+            this.buttonHermit.Location = new System.Drawing.Point(61, 426);
+            this.buttonHermit.Name = "buttonHermit";
+            this.buttonHermit.Size = new System.Drawing.Size(375, 23);
+            this.buttonHermit.TabIndex = 9;
+            this.buttonHermit.Text = "Hermit";
+            this.buttonHermit.UseVisualStyleBackColor = true;
+            // 
+            // buttonFortune
+            // 
+            this.buttonFortune.Location = new System.Drawing.Point(61, 455);
+            this.buttonFortune.Name = "buttonFortune";
+            this.buttonFortune.Size = new System.Drawing.Size(375, 23);
+            this.buttonFortune.TabIndex = 10;
+            this.buttonFortune.Text = "Fortune";
+            this.buttonFortune.UseVisualStyleBackColor = true;
+            // 
+            // buttonStrength
+            // 
+            this.buttonStrength.Location = new System.Drawing.Point(61, 484);
+            this.buttonStrength.Name = "buttonStrength";
+            this.buttonStrength.Size = new System.Drawing.Size(375, 23);
+            this.buttonStrength.TabIndex = 11;
+            this.buttonStrength.Text = "Strength";
+            this.buttonStrength.UseVisualStyleBackColor = true;
+            // 
+            // buttonHanged
+            // 
+            this.buttonHanged.Location = new System.Drawing.Point(61, 513);
+            this.buttonHanged.Name = "buttonHanged";
+            this.buttonHanged.Size = new System.Drawing.Size(375, 23);
+            this.buttonHanged.TabIndex = 12;
+            this.buttonHanged.Text = "Hanged";
+            this.buttonHanged.UseVisualStyleBackColor = true;
+            // 
+            // buttonDeath
+            // 
+            this.buttonDeath.Location = new System.Drawing.Point(61, 543);
+            this.buttonDeath.Name = "buttonDeath";
+            this.buttonDeath.Size = new System.Drawing.Size(375, 23);
+            this.buttonDeath.TabIndex = 13;
+            this.buttonDeath.Text = "Death";
+            this.buttonDeath.UseVisualStyleBackColor = true;
+            // 
+            // buttonTemperance
+            // 
+            this.buttonTemperance.Location = new System.Drawing.Point(61, 573);
+            this.buttonTemperance.Name = "buttonTemperance";
+            this.buttonTemperance.Size = new System.Drawing.Size(375, 23);
+            this.buttonTemperance.TabIndex = 14;
+            this.buttonTemperance.Text = "Temperance";
+            this.buttonTemperance.UseVisualStyleBackColor = true;
+            // 
+            // buttonDevil
+            // 
+            this.buttonDevil.Location = new System.Drawing.Point(61, 602);
+            this.buttonDevil.Name = "buttonDevil";
+            this.buttonDevil.Size = new System.Drawing.Size(375, 23);
+            this.buttonDevil.TabIndex = 15;
+            this.buttonDevil.Text = "Devil";
+            this.buttonDevil.UseVisualStyleBackColor = true;
+            // 
+            // buttonTower
+            // 
+            this.buttonTower.Location = new System.Drawing.Point(61, 632);
+            this.buttonTower.Name = "buttonTower";
+            this.buttonTower.Size = new System.Drawing.Size(375, 23);
+            this.buttonTower.TabIndex = 16;
+            this.buttonTower.Text = "Tower";
+            this.buttonTower.UseVisualStyleBackColor = true;
+            // 
+            // buttonStar
+            // 
+            this.buttonStar.Location = new System.Drawing.Point(61, 662);
+            this.buttonStar.Name = "buttonStar";
+            this.buttonStar.Size = new System.Drawing.Size(375, 23);
+            this.buttonStar.TabIndex = 17;
+            this.buttonStar.Text = "Star";
+            this.buttonStar.UseVisualStyleBackColor = true;
+            // 
+            // buttonMoon
+            // 
+            this.buttonMoon.Location = new System.Drawing.Point(61, 692);
+            this.buttonMoon.Name = "buttonMoon";
+            this.buttonMoon.Size = new System.Drawing.Size(375, 23);
+            this.buttonMoon.TabIndex = 18;
+            this.buttonMoon.Text = "Moon";
+            this.buttonMoon.UseVisualStyleBackColor = true;
+            // 
+            // buttonSun
+            // 
+            this.buttonSun.Location = new System.Drawing.Point(61, 722);
+            this.buttonSun.Name = "buttonSun";
+            this.buttonSun.Size = new System.Drawing.Size(375, 23);
+            this.buttonSun.TabIndex = 19;
+            this.buttonSun.Text = "Sun";
+            this.buttonSun.UseVisualStyleBackColor = true;
+            // 
+            // buttonJudgement
+            // 
+            this.buttonJudgement.Location = new System.Drawing.Point(61, 752);
+            this.buttonJudgement.Name = "buttonJudgement";
+            this.buttonJudgement.Size = new System.Drawing.Size(375, 23);
+            this.buttonJudgement.TabIndex = 20;
+            this.buttonJudgement.Text = "Judgement";
+            this.buttonJudgement.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(145, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 26);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Persona Fusion App";
+            // 
+            // textSearch
+            // 
+            this.textSearch.Location = new System.Drawing.Point(61, 70);
+            this.textSearch.Name = "textSearch";
+            this.textSearch.Size = new System.Drawing.Size(247, 20);
+            this.textSearch.TabIndex = 22;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(314, 68);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(121, 23);
+            this.buttonSearch.TabIndex = 23;
+            this.buttonSearch.Text = "Search Persona";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(244, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "or";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(165, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(167, 13);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Select Arcana of Desired Persona";
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 804);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.textSearch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonJudgement);
+            this.Controls.Add(this.buttonSun);
+            this.Controls.Add(this.buttonMoon);
+            this.Controls.Add(this.buttonStar);
+            this.Controls.Add(this.buttonTower);
+            this.Controls.Add(this.buttonDevil);
+            this.Controls.Add(this.buttonTemperance);
+            this.Controls.Add(this.buttonDeath);
+            this.Controls.Add(this.buttonHanged);
+            this.Controls.Add(this.buttonStrength);
+            this.Controls.Add(this.buttonFortune);
+            this.Controls.Add(this.buttonHermit);
+            this.Controls.Add(this.buttonJustice);
+            this.Controls.Add(this.buttonChariot);
+            this.Controls.Add(this.buttonLovers);
+            this.Controls.Add(this.buttonHierophant);
+            this.Controls.Add(this.buttonEmperor);
+            this.Controls.Add(this.buttonEmpress);
+            this.Controls.Add(this.buttonPriestess);
+            this.Controls.Add(this.buttonMagician);
+            this.Controls.Add(this.buttonFool);
+            this.Name = "MainMenu";
+            this.Text = "Main Menu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonFool;
+        private System.Windows.Forms.Button buttonMagician;
+        private System.Windows.Forms.Button buttonPriestess;
+        private System.Windows.Forms.Button buttonEmpress;
+        private System.Windows.Forms.Button buttonEmperor;
+        private System.Windows.Forms.Button buttonHierophant;
+        private System.Windows.Forms.Button buttonLovers;
+        private System.Windows.Forms.Button buttonChariot;
+        private System.Windows.Forms.Button buttonJustice;
+        private System.Windows.Forms.Button buttonHermit;
+        private System.Windows.Forms.Button buttonFortune;
+        private System.Windows.Forms.Button buttonStrength;
+        private System.Windows.Forms.Button buttonHanged;
+        private System.Windows.Forms.Button buttonDeath;
+        private System.Windows.Forms.Button buttonTemperance;
+        private System.Windows.Forms.Button buttonDevil;
+        private System.Windows.Forms.Button buttonTower;
+        private System.Windows.Forms.Button buttonStar;
+        private System.Windows.Forms.Button buttonMoon;
+        private System.Windows.Forms.Button buttonSun;
+        private System.Windows.Forms.Button buttonJudgement;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textSearch;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
