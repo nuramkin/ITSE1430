7c0f4ec459905b7dddec2004ed298aa37def5376
nicholas.uramkin <nicholas.uramkin@NEITD-123503>
2018-05-09 21:34:54 -0500

    fixed Final code

---
+++ Labs/Final/MovieLib.Web/Models/MovieViewModel.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * ITSE 1430
+ * Sample implementation
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace MovieLib.Web.Models
+{
+    /// <summary>Provides a view model for movies.</summary>
+    public class MovieViewModel : IValidatableObject
+    {
+        public int Id { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+        
+        [Display(Name = "Is Owned")]
+        public bool IsOwned { get; set; }
+
+        [Range(0, Int32.MaxValue)]
+        public int Length { get; set; }
+
+        public Rating Rating { get; set; }
+
+        [Range(1900, 2100, ErrorMessage = "Year must be >= 1900 and <= 2100")]//CR3 - added range to ReleaseYear with an error message to user
+        [Display(Name = "Release Year")]
+        public int ReleaseYear { get; set; }
+
+        public IEnumerable<ValidationResult> Validate ( ValidationContext validationContext ) => Enumerable.Empty<ValidationResult>();        
+    }
+}
